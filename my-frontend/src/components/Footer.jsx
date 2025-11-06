6ef430fbf0c2603ecb3da051d80bcb09761e2e2a
BhumikaCoder21 <bhumikanew21@gmail.com>
2025-11-06 22:08:23 +0530

    footer

---
+++ my-frontend/src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <footer className="bg-gray-800 text-white text-center py-4 mt-10">
+      <p>
+        © {new Date().getFullYear()} NE-FarmConnect | Empowering Northeast
+        Farmers
+      </p>
+    </footer>
+  );
+}
